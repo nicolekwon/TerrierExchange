80207b40921b61e2de72258a86ec9c26263105b1
yanchen01 <ychen01@bu.edu>
2019-11-29 00:39:45 -0500

    added objects for buttons and routes for different pages

--- TerrierExchange/app/src/main/java/com/example/terrierexchange/MainActivity.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.terrierexchange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create button objects for MainActivity page
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        // if clicks login button
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // make an Intent object for login page
+                Intent loginIntent = new Intent(getApplicationContext(), LoginPage.class);
+                // route the user to login page
+                startActivity(loginIntent);
+            }
+        });
+
+        // create button object for Registration page
+        Button registerBtn = (Button) findViewById(R.id.registerBtn);
+        // if clicks register button
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // make an Intent object for register page
+                Intent registerIntent = new Intent(getApplicationContext(), RegisterPage.class);
+                // route the user to register page
+                startActivity(registerIntent);
+            }
+        });
     }
 }
