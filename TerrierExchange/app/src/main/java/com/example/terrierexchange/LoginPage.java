dc5a33949c5f6f2c8db9314ee9fd3bdd39761bc9
yanchen01 <ychen01@bu.edu>
2019-11-30 11:04:42 -0500

    fixed main activity page and register page

--- TerrierExchange/app/src/main/java/com/example/terrierexchange/LoginPage.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/LoginPage.java
@@ -9,6 +9,6 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_register_page);
     }
 }
--- TerrierExchange/app/src/main/java/com/example/terrierexchange/RegisterPage.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/RegisterPage.java
@@ -9,6 +9,5 @@ public class RegisterPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_page);
     }
 }
