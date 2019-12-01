3a3ae3ff7c6a3b2a771ab1ae9cfd1835fb881cd2
yanchen01 <ychen01@bu.edu>
2019-12-01 13:31:28 -0500

    added login to dashboard and logout feature. Added firestore backend database

---
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/Dashboard.java
@@ -0,0 +1,57 @@
+package com.example.terrierexchange;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class Dashboard extends AppCompatActivity {
+    TextView name;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    Button logoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        name = findViewById(R.id.nameTextView);
+        logoutBtn = findViewById((R.id.logoutBtn));
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        // find the user's profile reference on database
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                // display user's name to screen
+                name.setText(documentSnapshot.getString("name"));
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();//logout
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
--- TerrierExchange/app/src/main/java/com/example/terrierexchange/MainActivity.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/MainActivity.java
@@ -1,34 +1,81 @@
 package com.example.terrierexchange;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    // declare objects
+    EditText emailInput, passwordInput;
+    Button loginBtn;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // create button objects for MainActivity page
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        // initialize button objects for login page
+        loginBtn = findViewById(R.id.loginBtn);
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        fAuth = FirebaseAuth.getInstance();
         // if clicks login button
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // make an Intent object for login page
-                Intent loginIntent = new Intent(getApplicationContext(), LoginPage.class);
-                // route the user to login page
-                startActivity(loginIntent);
+                // get the email and password input from user
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+
+                // check input validation
+                if(TextUtils.isEmpty(email)){
+                    emailInput.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    passwordInput.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    passwordInput.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                // authenticate the user
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            // route the user to dashboard by making new intent object for dashboard
+                            startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                        }else {
+                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
-        // create button object for Registration page
-        Button registerBtn = (Button) findViewById(R.id.registerBtn);
+        // initialize button object for Registration page
+        Button registerBtn = findViewById(R.id.registerBtn);
         // if clicks register button
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
