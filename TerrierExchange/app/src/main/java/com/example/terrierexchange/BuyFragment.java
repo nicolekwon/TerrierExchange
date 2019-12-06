9d308a7c589be161fc4c7e54990c6b30763e3078
yanchen01 <ychen01@bu.edu>
2019-12-05 21:25:54 -0500

    merge changes

--- TerrierExchange/app/src/main/java/com/example/terrierexchange/BuyFragment.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/BuyFragment.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.terrierexchange;
 
 
@@ -29,3 +30,48 @@ public class BuyFragment extends Fragment {
     }
 
 }
+=======
+package com.example.terrierexchange;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BuyFragment extends Fragment {
+
+
+    public BuyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buy, container, false);
+        Button casbutton = (Button) view.findViewById(R.id.casbutton);
+        Button kilachandbutton = (Button) view.findViewById(R.id.kilachandbutton);
+        Button combutton = (Button) view.findViewById(R.id.combutton);
+        Button cgsbutton = (Button) view.findViewById(R.id.cgsbutton);
+        Button engbutton = (Button) view.findViewById(R.id.engbutton);
+        Button shabutton = (Button) view.findViewById(R.id.shabutton);
+        Button qstbutton = (Button) view.findViewById(R.id.qstbutton);
+        Button sarbutton = (Button) view.findViewById(R.id.sarbutton);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
+>>>>>>> cace3cbdd2cb0ffe3bcbaa4cc022ed786a7537ee
--- TerrierExchange/app/src/main/java/com/example/terrierexchange/SellFragment.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/SellFragment.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.terrierexchange;
 
 
@@ -29,3 +30,42 @@ public class SellFragment extends Fragment {
     }
 
 }
+=======
+package com.example.terrierexchange;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SellFragment extends Fragment {
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+
+
+    public SellFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sell, container, false);
+    }
+
+}
+>>>>>>> cace3cbdd2cb0ffe3bcbaa4cc022ed786a7537ee
