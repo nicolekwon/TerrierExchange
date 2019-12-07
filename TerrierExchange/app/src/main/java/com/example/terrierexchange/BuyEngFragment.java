a96837463cb3766c429f8f2cc4f0ff1afbd5516d
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2019-12-07 16:52:31 -0500

    Added more set up for linked lists

--- TerrierExchange/app/src/main/java/com/example/terrierexchange/BuyEngFragment.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/BuyEngFragment.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -20,12 +24,21 @@ public class BuyEngFragment extends Fragment {
         // Required empty public constructor
     }
 
+    ListView listView;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_buy_eng, container, false);
+        listView = (ListView) view.findViewById(R.id.listviewENG);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("COM example 1");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buy_eng, container, false);
+        return view;
     }
 
 }
