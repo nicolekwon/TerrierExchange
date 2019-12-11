dcf2957a4acecc3a6bb4a25618097806d0747537
austinnegron <negrona@bu.edu>
2019-12-11 05:53:15 -0500

    add final confirmation page, rearrange interface

--- TerrierExchange/app/src/main/java/com/example/terrierexchange/Check.java
+++ TerrierExchange/app/src/main/java/com/example/terrierexchange/Check.java
@@ -4,10 +4,12 @@ package com.example.terrierexchange;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -24,8 +26,20 @@ public class Check extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_check, container, false);
+        Button purchase_btn = (Button) view.findViewById(R.id.purchase_btn);
+        purchase_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                //BuyCasFragment buyCasFragment = new BuyCasFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout,new final_confirmation());
+                transaction.commit();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check, container, false);
+        return view;
     }
 
 }
